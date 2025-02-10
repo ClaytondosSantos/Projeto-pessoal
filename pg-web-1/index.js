dd54adabea5b004a979d7bf1f8094aa35bffc8af
ClaytondosSantos <clayton_ssantos@outlook.com>
2025-02-10 12:04:11 -0300

    alterações feitas

--- pg-web-1/index.js
+++ pg-web-1/index.js
@@ -1,7 +1,15 @@
 const elements = document.querySelector(".btn-cadastro");
+function comeceGratis(){
+    console.log("CADADASTRO PARA INÍCIO GRATUITO");
+  }  
 
-
-function ok(){
-    console.log("ok");
+function cadastro(){
+  alert("EXECUTANDO O CADASTRO");
 }  
+
+const menu = document.querySelector(".menu") 
+menu.addEventListener("click", function(event) {
+  console.log(event);
+});
+
 
